816c43c3f90a7f4b918fb17bf03b906d3a4da2ac
hugodiloy <89636652+Brasego@users.noreply.github.com>
2022-02-08 16:41:50 +0100

    Redux setup + Redirection from signin to homepage for signed in users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+
 import "./App.css";
+
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
+import { setCurrentUser } from "./redux/user/user.actions";
 //Routing:
 //React-router-dom V6 replaced the Switch Element and it's overall syntax. So my code is a bit different from what's in the course
 //What changed :
@@ -16,36 +19,24 @@ import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 //We can have access to browser's history with props. (See HomePage Component)
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         //Using .data() on a snapShot object will give us its data
         userRef.onSnapshot((snapShot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
-            },
-            () => {
-              console.log(this.state);
-            }
-          );
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
       }
+
+      setCurrentUser(userAuth);
     });
   }
 
@@ -56,15 +47,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndSignUp />} />
+          <Route
+            path="/signin"
+            element={
+              this.props.currentUser ? <Navigate to="/" /> : <SignInAndSignUp />
+            }
+          />
         </Routes>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
